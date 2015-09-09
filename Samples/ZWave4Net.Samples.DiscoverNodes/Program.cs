d87c0d9010379bacc425ab3983f97d93dce671b3
Rob Lans <r.lans@upcmail.nl>
2015-09-09 14:04:39 +0200

    Added DiscoverNodes sample project

---
+++ Samples/ZWave4Net.Samples.DiscoverNodes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZWave4Net;
+
+namespace ZWave4Net.Samples.DiscoverNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Platform.Log = Logger.LogMessage;
+
+            var port = new SerialPort(System.IO.Ports.SerialPort.GetPortNames().First());
+            var driver = new ZWaveDriver(port);
+
+            driver.Open().Wait();
+            try
+            {
+                Console.ReadLine();
+            }
+            finally
+            {
+                driver.Close();
+            }
+        }
+    }
+}
