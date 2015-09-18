5cfe80ec7cb05bea1a102636d244f24726d2cbab
Rob Lans <r.lans@upcmail.nl>
2015-09-18 23:39:03 +0200

    None

---
+++ ZWave4Net.Driver/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWave4Net.Driver
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+    };
+}
---
+++ ZWave4Net.Driver/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZWave4Net.Driver
+{
+    public static class Platform
+    {
+        public static Action<LogLevel, string> LogMessage = (_, __) => { };
+    }
+}
