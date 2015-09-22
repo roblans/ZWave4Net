baad30d3e9f56aeee097641f9091919430fad22d
Rob Lans <r.lans@upcmail.nl>
2015-09-22 19:22:02 +0200

    Added ZWaveDriver

---
+++ Samples/net/ZWaveDriverSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZWave.Driver;
+
+namespace ZWaveDriverSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var portName = System.IO.Ports.SerialPort.GetPortNames().First();
+
+            var driver = new ZWaveDriver(portName);
+
+            driver.Open();
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+                driver.Close();
+            }
+        }
+    }
+}
---
+++ ZWave/Driver/ZWaveDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZWave.Driver.Communication;
+
+namespace ZWave.Driver
+{
+    public class ZWaveDriver
+    {
+        public readonly ZWaveChannel Channel;
+
+        public ZWaveDriver(ISerialPort port)
+        {
+            Channel = new ZWaveChannel(port);
+        }
+
+#if NET || WINDOWS_UWP
+        public ZWaveDriver(string portName)
+        {
+            Channel = new ZWaveChannel(portName);
+        }
+#endif
+
+        public void Open()
+        {
+            Channel.Open();
+        }
+
+        public void Close()
+        {
+            Channel.Close();
+        }
+    }
+}
