ef3321bb7b6e6920f7d3217d50d131ac96ff0292
Rob Lans <r.lans@upcmail.nl>
2015-09-28 13:27:13 +0200

    Develop

--- Build/SetAssemblyVersion.ps1
+++ Build/SetAssemblyVersion.ps1
@@ -1,60 +1,51 @@
-# SetAssemblyVersion.ps1
-#
-# http://www.luisrocha.net/2009/11/setting-assembly-version-with-windows.html
-# http://blogs.msdn.com/b/dotnetinterop/archive/2008/04/21/powershell-script-to-batch-update-assemblyinfo-cs-with-new-version.aspx
-# http://jake.murzy.com/post/3099699807/how-to-update-assembly-version-numbers-with-teamcity
-# https://github.com/ferventcoder/this.Log/blob/master/build.ps1#L6-L19
-
 Param(
-    [string]$path=$pwd
+    [Parameter(Mandatory=$False)]
+    [string]$path=$pwd,
+    [Parameter(Mandatory=$False,Position=1)]
+    [string]$Revision=$env:BUILD_BUILDID
 )
-
+ 
 function Help {
     "Sets the AssemblyVersion and AssemblyFileVersion of AssemblyInfo.cs files`n"
-    ".\SetAssemblyVersion.ps1 [VersionNumber] -path [SearchPath]`n"
-    "   [VersionNumber]     The version number to set, for example: 1.1.9301.0"
-    "   [SearchPath]        The path to search for AssemblyInfo files.`n"
+    ".\SetAssemblyVersion.ps1 -Revision [Revision] -path [SearchPath]`n"
+    "   [Revision]     The assembly revision number to set. Revision is the last number in the 4-part version number. E.g. 4 in 1.2.3.4"
+    "   [SearchPath]   The path to search for AssemblyInfo files. Defaults to current folder. `n"
 }
+
 function Update-SourceVersion
 {
-    Param ([string]$Version)
-    $NewVersion = 'AssemblyVersion("' + $Version + '")';
-    $NewFileVersion = 'AssemblyFileVersion("' + $Version + '")';
-
+    Param ([string]$rev)
+    
     foreach ($o in $input) 
     {
-        Write-Host "Updating  '$($o.FullName)' -> $Version"
-    
-        $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-        $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-        $assemblyVersion = 'AssemblyVersion("' + $version + '")';
-        $fileVersion = 'AssemblyFileVersion("' + $version + '")';
+        $versionPattern = "([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]*)"
+        $replacePattern = "`$1.`$2`.`$3.$rev"
+
+        Write-Host "Updating revision of '$($o.FullName)' to $rev"
+
+        $assemblyVersionPattern = "AssemblyVersion\(`"$versionPattern`"\)"
+        $fileVersionPattern = "AssemblyFileVersion\(`"$versionPattern`"\)"
+        $assemblyVersion = "AssemblyVersion(`"$replacePattern`")";
+        $fileVersion = "AssemblyFileVersion(`"$replacePattern`")";
         
-        (Get-Content $o.FullName) | ForEach-Object  { 
+        (Get-Content -encoding UTF8 $o.FullName) | ForEach-Object  { 
            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
            % {$_ -replace $fileVersionPattern, $fileVersion }
         } | Out-File $o.FullName -encoding UTF8 -force
     }
 }
-function Update-AllAssemblyInfoFiles ( $version )
+function Update-AllAssemblyInfoFiles ( $rev )
 {
-    Write-Host "Searching '$path'"
-   foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) 
-   {
-        get-childitem $path -recurse |? {$_.Name -eq $file} | Update-SourceVersion $version ;
-   }
+    Write-Host "Searching '$path'"   
+    Get-ChildItem -Path $path -Recurse "AssemblyInfo.cs" `
+    | ? { $_.fullname -notmatch '\\packages\\' } `
+    | Update-SourceVersion -rev $rev
 }
 
-# validate arguments 
-if ($args -ne $null) {
-    $version = $args[0]
-    if (($version -eq '/?') -or ($version -notmatch "[0-9]+(\.([0-9]+|\*)){1,3}")) {
-        Help
-        exit 1;
-    }
-} else {
+# validate arguments   
+if (($Revision -eq '/?') -or ($Revision -notmatch "[0-9]+")) {
     Help
     exit 1;
 }
-
-Update-AllAssemblyInfoFiles $version
+ 
+Update-AllAssemblyInfoFiles $Revision
