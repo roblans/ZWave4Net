62cb41a6bb9b9eae18acbfab517b6435b5c309e0
Rob Lans <r.lans@upcmail.nl>
2015-10-09 13:29:19 +0200

    Develop

--- ZWave/Channel/Protocol/NodeCommandCompleted.cs
+++ ZWave/Channel/Protocol/NodeCommandCompleted.cs
@@ -10,27 +10,17 @@ namespace ZWave.Channel.Protocol
     {
         public readonly byte CallbackID;
         public readonly TransmissionState TransmissionState;
-        public readonly byte UnknownByte1;
-        public readonly byte UnknownByte2;
 
         public NodeCommandCompleted(byte[] payload) : 
             base(FrameHeader.SOF, MessageType.Request, Channel.Function.SendData)
         {
             CallbackID = payload[0];
             TransmissionState = (TransmissionState)payload[1];
-            if (payload.Length > 2)
-            {
-                UnknownByte1 = payload[2];
-            }
-            if (payload.Length > 3)
-            {
-                UnknownByte2 = payload[3];
-            }
         }
 
         public override string ToString()
         {
-            return string.Concat(base.ToString(), " ", $"CallbackID:{CallbackID}, {TransmissionState}, {UnknownByte1}?, {UnknownByte2}?");
+            return string.Concat(base.ToString(), " ", $"CallbackID:{CallbackID}, {TransmissionState}");
         }
     }
 }
