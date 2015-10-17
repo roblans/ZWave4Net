2f500546281c3977b7b679780fab983d911e3a04
Rob Lans <r.lans@upcmail.nl>
2015-10-17 13:06:25 +0200

    Added Danfoss Thermostat

---
+++ ZWave/Devices/Danfoss/RadiatorThermostat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ZWave.CommandClasses;
+
+namespace ZWave.Devices.Danfoss
+{
+    public class RadiatorThermostat : BatteryDevice
+    {
+        public event EventHandler<SetpointEventArgs> SetPointChanged;
+
+        public RadiatorThermostat(Node node)
+            : base(node)
+        {
+            node.GetCommandClass<ThermostatSetpoint>().Changed += ThermostatSetpoint_Changed;
+
+        }
+
+        public async Task<Setpoint> GetSetpoint()
+        {
+            var report = await Node.GetCommandClass<ThermostatSetpoint>().Get(ThermostatSetpointType.Heating);
+            return new Setpoint(report.Value, report.Scale == 0 ? Unit.Celsius : Unit.Fahrenheit);
+        }
+
+        public async Task SetSetpoint(float value)
+        {
+            await Node.GetCommandClass<ThermostatSetpoint>().Set(ThermostatSetpointType.Heating, value);
+        }
+
+        private void ThermostatSetpoint_Changed(object sender, ReportEventArgs<ThermostatSetpointReport> e)
+        {
+            var setpoint = new Setpoint(e.Report.Value, e.Report.Scale == 0 ? Unit.Celsius : Unit.Fahrenheit);
+            OnSetPointChanged(new SetpointEventArgs(setpoint));
+        }
+
+        protected virtual void OnSetPointChanged(SetpointEventArgs e)
+        {
+            SetPointChanged?.Invoke(this, e);
+        }
+    }
+}
