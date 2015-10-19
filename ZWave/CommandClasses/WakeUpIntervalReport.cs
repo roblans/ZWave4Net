80c45aca16b0892a74c77934e13c21564a3a8e06
Rob Lans <r.lans@upcmail.nl>
2015-10-19 11:41:05 +0200

    Fixed WakeUpIntervalReport payload bug

--- ZWave/CommandClasses/WakeUpIntervalReport.cs
+++ ZWave/CommandClasses/WakeUpIntervalReport.cs
@@ -3,20 +3,36 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using ZWave.Channel;
+using ZWave.Channel.Protocol;
 
 namespace ZWave.CommandClasses
 {
     public class WakeUpIntervalReport : NodeReport
     {
-        public readonly TimeSpan Interval;
+        public readonly TimeSpan Interval = TimeSpan.Zero;
         public readonly byte TargetNodeID;
 
         internal WakeUpIntervalReport(Node node, byte[] payload) : base(node)
         {
-            // 3 bytes for interval
-            var interval = PayloadConverter.ToUInt32(new byte[] { 0 }.Concat(payload).ToArray());
-            Interval = TimeSpan.FromSeconds(interval);
-            TargetNodeID = payload[3];
+            if (payload.Length >= 4)
+            {
+                // 3 bytes for interval
+                var interval = PayloadConverter.ToUInt32(new byte[] { 0 }.Concat(payload).ToArray());
+                Interval = TimeSpan.FromSeconds(interval);
+
+                // one byte for targetnode
+                TargetNodeID = payload[3];
+                return;
+            }
+            
+            // some interval reports received are validly formatted(proper checksum, etc.) but only have length of 1 byte (0x00). Not sure what this means
+            if (payload.Length == 1 && payload[0] == 0x00)
+            {
+                Interval = TimeSpan.Zero;
+                TargetNodeID = 0;
+                return;
+            }
+            throw new ProtocolException($"Node:{node:D3}, Payload:{BitConverter.ToString(payload)}");
         }
 
         public override string ToString()
