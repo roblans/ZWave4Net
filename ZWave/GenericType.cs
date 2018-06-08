222f83c2bdcd548b27bc7573906a7c6046648d33
Richard Thombs <richard@gearstone.uk>
2018-06-08 12:51:52 +0100

    Update GenericType enum in accordance with spec

--- ZWave/GenericType.cs
+++ ZWave/GenericType.cs
@@ -7,12 +7,13 @@ namespace ZWave
     public enum GenericType : byte
     {
         Unknown = 0x00,
-        PortableRemote = 0x01,
+        GenericController = 0x01,
         StaticController = 0x02,
         AVControlPoint = 0x03,
-        RoutingSlave = 0x04,
-        Display = 0x06,
-        GarageDoor = 0x07,
+        Display = 0x04,
+        NetworkExtender = 0x05,
+        Appliance = 0x06,
+        SensorNotification = 0x07,
         Thermostat = 0x08,
         WindowCovering = 0x09,
         RepeaterSlave = 0x0F,
@@ -20,13 +21,17 @@ namespace ZWave
         SwitchMultiLevel = 0x11,
         SwitchRemote = 0x12,
         SwitchToggle = 0x13,
+        ZipNode = 0x15,
+        Ventilation = 0x16,
+        SecurityPanel = 0x17,
+        WallController = 0x18,
         SensorBinary = 0x20,
         SensorMultiLevel = 0x21,
-        WaterControl = 0x22,
         MeterPulse = 0x30,
+        Meter = 0x31,
         EntryControl = 0x40,
         SemiInteroperable = 0x50,
-        SmokeDetector = 0xA1,
+        SensorAlarm = 0xA1,
         NonInteroperable = 0xFF
     }
 }
