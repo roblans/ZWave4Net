94840f5f974941ad537746672b3d26db93f90db9
jdomnitz <380352+jdomnitz@users.noreply.github.com>
2023-05-08 18:21:35 -0400

    More command classes + SwitchToggleBinary

--- Source/ZWave/CommandClasses/SwitchBinaryReport.cs
+++ Source/ZWave/CommandClasses/SwitchBinaryReport.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using ZWave.Channel;
 using ZWave.Channel.Protocol;
 
 namespace ZWave.CommandClasses
 {
     public class SwitchBinaryReport : NodeReport
     {
-        public readonly bool Value;
+        private const byte UNKNOWN = 0xFE;
+
+        public readonly bool? CurrentValue;
+        public readonly bool? TargetValue;
+        public readonly TimeSpan Duration;
+
 
         internal SwitchBinaryReport(Node node, byte[] payload) : base(node)
         {
@@ -18,12 +19,35 @@ namespace ZWave.CommandClasses
             if (payload.Length < 1)
                 throw new ReponseFormatException($"The response was not in the expected format. {GetType().Name}: Payload: {BitConverter.ToString(payload)}");
 
-            Value = payload[0] == 0xFF;
+            if (payload[0] == UNKNOWN)
+                CurrentValue = null;
+            else
+                CurrentValue = payload[0] != 0x0; //Values 0x1 - 0xFF = On
+
+            //Version 2
+            if (payload.Length > 2)
+            {
+                if (payload[1] == UNKNOWN)
+                    TargetValue = null;
+                else
+                    TargetValue = payload[1] != 0x0; //Values 0x1 - 0xFF = On
+                if (payload[2] == UNKNOWN || payload[2] == 0x0)
+                    Duration = TimeSpan.Zero;
+                if (payload[2] < 0x80)
+                    Duration = new TimeSpan(0, 0, payload[2]);
+                else
+                    Duration = new TimeSpan(0, payload[2] - 0x80, 0);
+            }
+            else
+            {
+                Duration = TimeSpan.Zero;
+                TargetValue = CurrentValue;
+            }
         }
 
         public override string ToString()
         {
-            return $"Value:{Value}";
+            return $"Value:{Value},Duration:{Duration}";
         }
     }
 }
